58a7b6512dea41cd21f3ed20629e25bcbc1c8676
Jeff <jeffinho2002@live.com>
2025-01-30 16:19:58 -0300

    +

---
+++ Mundo 3/11 - Listas (P1)/ex83.py
@@ -0,0 +1,17 @@
+expression = input("Digite uma expressão: ")
+stack = []
+
+for char in expression:
+    if char == '(':
+        stack.append('(')
+    elif char == ')':
+        if len(stack) > 0:
+            stack.pop()
+        else:
+            stack.append(')')
+            break
+
+if len(stack) == 0:
+    print("A expressão está correta!")
+else:
+    print("A expressão está incorreta!")
